2bc55699ef4c91b391d34b34b569a6a63f0c06dc
Ghadeer <ghaora@gmail.com>
2021-12-17 01:06:56 +0200

    correzioni -testo

--- myscript.js
+++ myscript.js
@@ -24,18 +24,18 @@ const text = [
 ];
 
 let itemsContent = '';
-let elementActive = 1;
+let elementActive = 0;
 
 
 for(let i=0; i<items.length; i++){
     itemsContent += `
-      <div class="ms_mainPhoto col-10" id="item-${i}">
+      <div class="ms_mainPhoto col-10 item" id="item-${i}">
           <img src="${items[i]}">
-      </div>
-      <div class="ms_textContainer align-self-end m-5 text-end">
-          <h1 class="title text-white d-flex justify-content-end me-3 id="title-${i}">${title[i]}</h1>
-          <p class="text text-white d-flex justify-conetid="text-${i}> ${text[i]}</p>
-      </div>`;
+          <div class="text justify-conetid-end">
+          <h1 class="title text-white d-flex justify-content-end me-3 item">${title[i]}</h1>
+          <p class="text text-white d-flex flex-row-reverse item"> ${text[i]}</p>
+          </div>
+       </div>`;
     
 
 }
@@ -62,7 +62,7 @@ prev.addEventListener('click', function(){
    
 });
 next.addEventListener('click', function(){
-    if(elementActive<4){
+    if(elementActive<items.length-1){
         elementActive++;
         itemActive.classList.remove('active');
         itemActive= document.getElementById('item-'+elementActive);
